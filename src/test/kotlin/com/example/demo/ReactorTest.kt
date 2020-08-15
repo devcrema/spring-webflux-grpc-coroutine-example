0aa49335b993891660c90c4fea5af79af5fe785f
devcrema <devcrema@gmail.com>
2020-08-15 18:44:29 +0900

    test reactor timeout retry

--- src/test/kotlin/com/example/demo/ReactorTest.kt
+++ src/test/kotlin/com/example/demo/ReactorTest.kt
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
+import java.lang.RuntimeException
 import java.lang.Thread.sleep
+import java.time.Duration
 
 @Suppress("BlockingMethodInNonBlockingContext")
 class ReactorTest {
@@ -49,22 +51,47 @@ class ReactorTest {
         Mono.zip(
                 Mono.just(1)
                         .map {
-                            sleep(5000)
+                            sleep(1000)
                             it.logThis()
                         }.subscribeOn(sub1),
                 Mono.just(2)
                         .map {
-                            sleep(5000)
+                            sleep(2000)
                             it.logThis()
                         }.subscribeOn(sub2),
                 Mono.just(3)
                         .map {
-                            sleep(3000)
+                            sleep(1000)
                             it.logThis()
                         }.subscribeOn(sub2)
         )
                 .map { it.t1 + it.t2 + it.t3 }
-                .block().also { it?.logThis() } // result in 8000 (5000 + 3000) millis.
+                .block().also { it?.logThis() } // result in 3000 (1000 + 2000) millis.
+    }
+
+    @Test
+    fun reactorTimeoutRetryTest(){
+        Mono.just(1)
+                .flatMap {
+                    log.info("wait...")
+                    Mono.just(2)
+                            .map { sleep(2000) }
+                            .subscribeOn(Schedulers.newElastic("2")) // with same thread. can not timeout catch.
+                }
+                /**
+                 * must no signal in duration. if some signal received then no timeout exception occur.
+                 * if timeout in same thread, timeout start to catch after sleep 2000. so does not throw any timeout exception.
+                 * because stream is sequentially triggered. Mono.just(2).map{sleep(2000)} -> timeout(1000) -> already received signal -> no timeout
+                 */
+                .timeout(Duration.ofMillis(1000), Mono.error(NullPointerException())) // fallback to custom error
+                .retry(2) // on error signal received then retry.
+//                .timeout(Duration.ofMillis(1000)) // java.util.concurrent.TimeoutException
+                .subscribeOn(Schedulers.newElastic("1"))
+                .onErrorResume {
+                    log.info(it.message)
+                    Mono.empty()
+                }
+                .block()
     }
 
     @Test
