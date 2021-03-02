7800462f93a49a85badc0591ac116d8754c2d3c1
Sungeun Park <devcrema@gmail.com>
2021-03-02 19:58:29 +0900

    :bulb: add blocking repository codes and comment

--- src/main/kotlin/com/example/demo/TestRepository.kt
+++ src/main/kotlin/com/example/demo/TestRepository.kt
@@ -8,16 +8,27 @@ import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Repository
-class TestRepository(private val testReactiveRepository: TestReactiveRepository){
+class TestRepository(
+        private val testReactiveRepository: TestReactiveRepository,
+        private val testJpaRepository: TestJpaRepository
+){
 
     suspend fun findById(): String? = testReactiveRepository.findById().awaitFirstOrNull()
     suspend fun findAll(): Flow<String> = testReactiveRepository.findAll().asFlow()
 
+    // unlike Mono, you can just call it with null
+    suspend fun findByIdWithJpa(): String? = testJpaRepository.findByIdOrNull()
+    suspend fun findAllWithJpa(): List<String> = testJpaRepository.findAll()
 }
 
 @Repository
 class TestReactiveRepository{
-
     fun findAll(): Flux<String> = Flux.just("this ", " is ", " coroutine", " flow")
     fun findById(): Mono<String> = Mono.just("hello world!!!")
+}
+
+@Repository
+class TestJpaRepository {
+    fun findAll(): List<String> = listOf("this ", " is ", " coroutine ", " flow ")
+    fun findByIdOrNull(): String? = "hell world!!!"
 }
